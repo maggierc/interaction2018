d094c5835e2b469998c43f49a72740b31020e702
maggierc <31423327+maggierc@users.noreply.github.com>
2018-09-08 15:45:56 -0500

    Added 4-7
    changed from inline-blocks to relative/float left

--- mouse-events/jquery.js
+++ mouse-events/jquery.js
@@ -12,11 +12,56 @@ $(document).ready(function(){
 
   });
 
-  $("#three").click(function(){
-      $("#three1").fadeOut();
-      $("#three2").fadeOut();
+  // $("#three").click(function(){
+  //     $("#three1").fadeOut();
+  //     $("#three2").fadeOut();
+  //
+  // });
+
+  $("#three").hover(function(){
+      $("#three1").animate({
+    	stroke:"red",
+	    }, 200);
 
   });
 
+  $("#four").mouseenter(function(){
+      $("#four1").fadeOut();
+      $("#four2").fadeOut();
+
+  });
+
+  $("#four").mouseleave(function(){
+      $("#four1").fadeIn();
+      $("#four2").fadeIn();
+
+  });
+
+  $("#five").hover(function(){
+      $("#five1").css('transform', 'rotate(-6deg)');
+      $("#five2").css('transform', 'rotate(6deg)');
+
+  });
+
+  $('#six').mouseover(function(){
+
+    	$("#six1").animate({
+    	opacity:".2",
+	    }, 200);
+      $("#six2").animate({
+    	opacity:".2",
+	    }, 200);
+  });
+
+  $('#six').mouseout(function(){
+
+    $("#six1").animate({
+    opacity:"1",
+    }, 200);
+    $("#six2").animate({
+    opacity:"1",
+    }, 200);
+
+    });
 
 });
