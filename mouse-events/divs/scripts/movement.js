37cd4d9ca9fc7be752715923391476e7abcc7d0f
maggierc <31423327+maggierc@users.noreply.github.com>
2018-09-12 12:13:59 -0500

    hover animation added ABC

--- mouse-events/divs/scripts/movement.js
+++ mouse-events/divs/scripts/movement.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#A").hover(function(){
+  $("#A").mouseover(function(){
     $("div#A .type1").toggleClass("rotateAnimate1");
     $("div#A .type2").toggleClass("rotateAnimate2");
     $("div#A .type3").toggleClass("rotateAnimateSlow1");
@@ -8,4 +8,56 @@ $(document).ready(function(){
 
     $("div#A div").css('background-color', '#f7f7f7');
   });
+
+  $("#A").mouseout(function(){
+    $("div#A .type1").toggleClass("rotateAnimate1");
+    $("div#A .type2").toggleClass("rotateAnimate2");
+    $("div#A .type3").toggleClass("rotateAnimateSlow1");
+    $("div#A .type4").toggleClass("rotateAnimateSlow2");
+    $("div#A").toggleClass("pause");
+
+    $("div#A div").css('background-color', '#7971ea');
+  });
+
+  $("#B").mouseover(function(){
+    $("div#B .type1").toggleClass("rotateAnimate1");
+    $("div#B .type2").toggleClass("rotateAnimate2");
+    $("div#B .type3").toggleClass("rotateAnimateSlow1");
+    $("div#B .type4").toggleClass("rotateAnimateSlow2");
+    $("div#B").toggleClass("pause");
+
+    $("div#B div").css('background-color', '#f7f7f7');
+  });
+
+  $("#B").mouseout(function(){
+    $("div#B .type1").toggleClass("rotateAnimate1");
+    $("div#B .type2").toggleClass("rotateAnimate2");
+    $("div#B .type3").toggleClass("rotateAnimateSlow1");
+    $("div#B .type4").toggleClass("rotateAnimateSlow2");
+    $("div#B").toggleClass("pause");
+
+    $("div#B div").css('background-color', '#7971ea');
+  });
+
+  $("#C").mouseover(function(){
+    $("div#C .type1").toggleClass("rotateAnimate1");
+    $("div#C .type2").toggleClass("rotateAnimate2");
+    $("div#C .type3").toggleClass("rotateAnimateSlow1");
+    $("div#C .type4").toggleClass("rotateAnimateSlow2");
+    $("div#C").toggleClass("pause");
+
+    $("div#C div").css('background-color', '#f7f7f7');
+  });
+
+  $("#C").mouseout(function(){
+    $("div#C .type1").toggleClass("rotateAnimate1");
+    $("div#C .type2").toggleClass("rotateAnimate2");
+    $("div#C .type3").toggleClass("rotateAnimateSlow1");
+    $("div#C .type4").toggleClass("rotateAnimateSlow2");
+    $("div#C").toggleClass("pause");
+
+    $("div#C div").css('background-color', '#7971ea');
+  });
+
+
 });
