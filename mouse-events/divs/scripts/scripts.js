373b1d9a070da224dc919a4402ee026b2cf07947
maggierc <31423327+maggierc@users.noreply.github.com>
2018-09-11 23:45:44 -0500

    added layer div through I

--- mouse-events/divs/scripts/scripts.js
+++ mouse-events/divs/scripts/scripts.js
@@ -8,6 +8,14 @@ $(document).ready(function(){
     $("div#A div").css('background-color', '#7c38a0');
   });
 
+  $("#B").hover(function(){
+    $("div#B .type1").toggleClass("rotateAnimate1");
+    $("div#B .type2").toggleClass("rotateAnimate2");
+    $("div#B .type3").toggleClass("rotateAnimateSlow1");
+    $("div#B .type4").toggleClass("rotateAnimateSlow2");
+    $("div#B div").css('background-color', '#7c38a0');
+  });
+
 
 
 
