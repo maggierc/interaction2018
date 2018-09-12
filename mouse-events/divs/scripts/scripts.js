fbb86082261fb97564b25754a9ac7f0b682b440e
maggierc <31423327+maggierc@users.noreply.github.com>
2018-09-11 23:33:02 -0500

    making it work
    - added type1/2/3/4 coding for jquery
    - toggle background color added

--- mouse-events/divs/scripts/scripts.js
+++ mouse-events/divs/scripts/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
   $("#A").hover(function(){
-    $("div#A").toggleClass("rotateAnimate1");
-    $("div#A").toggleClass("rotateAnimate2");
-    // $("div#O div.OSlow1").toggleClass("rotateAnimateSlow1", 'slow');
-    // $("div#O div.OSlow2").toggleClass("rotateAnimateSlow2", 'slow');
-
+    $("div#A .type1").toggleClass("rotateAnimate1");
+    $("div#A .type2").toggleClass("rotateAnimate2");
+    $("div#A .type3").toggleClass("rotateAnimateSlow1");
+    $("div#A .type4").toggleClass("rotateAnimateSlow2");
+    $("div#A div").css('background-color', '#7c38a0');
   });
 
 
