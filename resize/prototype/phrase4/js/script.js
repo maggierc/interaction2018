095a869450d3f648b053028f4361957691db8d0b
maggierc <31423327+maggierc@users.noreply.github.com>
2018-12-02 22:51:41 -0600

    prototype with landing page

--- resize/prototype/phrase4/js/script.js
+++ resize/prototype/phrase4/js/script.js
@@ -1,31 +1,14 @@
 $(document).ready(function() {
 
-	var boxOne = false;
-	var boxTwo = false;
-	var boxThree = false;
-	var boxFour = false;
-
-
 	$('#box').click('click touchstart', function() {
 	$( this ).css( "width", "20%" );
 
 	});
 
-	// $('div > p').hover(function(){
-	// 	$('body').css('opacity','0');
-	// 	setTimeout(function () {
-	// 		window.location.replace("../phrase1/index.html");
-	// 	}, 2000);
-	// });
 
 
-
-	$(window).resize(function() {
-		if (window.innerWidth>475 && window.innerWidth<480) {
-
-		}
-
-
+	$('#startButton').click(function(){
+		$('.introScreen').css("opacity", "0");
 	});
 
 });
